bebf290c9156deec5e02a1b88517ec60cc1352e5
Alexzeboulet <alex.violettee@gmail.com>
2025-09-16 08:51:28 +0200

    Exo3

--- Exo1/script.js
+++ Exo1/script.js
@@ -6,3 +6,21 @@
 
 // DEMANDER UNE SAISIE A L'UTILISATEUR prompt("Hello World");
 
+/* Exercice 1 pour demander une température 
+var températureCelsius = prompt("Entrez une température en Celsius :");
+var températureFahrenheit = (températureCelsius * 9/5) + 32;
+
+document.write(températureCelsius + "°C est égal à " + températureFahrenheit + "°F") */
+
+/* Exercice 2 pour demander la longueur et la largeur d'un rectangle ; 
+
+var longueur = prompt("Entrez la longueur du rectangke");
+var largeur = prompt("Entrez la largeur du rectangle");
+document.write("Le rectangle de longueur " + longueur + " et de largeur " + largeur + " a une aire de " + (longueur * largeur)); */
+
+
+/* Exercice 3 pour demander un prénom et un nom et les concaténer et les afficher;
+var prénom = prompt("Entrez votre prénom");
+var nom = prompt("Entrez votre Nom");
+document.write ("Bonjour " + prénom + " " + nom);
+*/ 
