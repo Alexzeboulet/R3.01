59d90e362607c680ad29f9e8770c204c5061ea27
Alexzeboulet <alex.violettee@gmail.com>
2025-09-16 08:35:15 +0200

    exerciceAffichage

---
+++ Exo1/script.js
@@ -0,0 +1,8 @@
+// AFFICHER DANS LA CONSOLE console.log("Hello World");
+
+// AFFICHER DANS LA PAGE WEB document.write("Hello World");
+
+// AFFICHER DANS UNE BOITE DE DIALOGUE alert("Hello World");
+
+// DEMANDER UNE SAISIE A L'UTILISATEUR prompt("Hello World");
+
